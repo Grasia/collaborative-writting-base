f3b82007f02155473f8c30c8df2fde3824b61018
Pablo Ojanguren <pablojan@gmail.com>
2018-04-26 10:31:27 +0200

    Id de nodo con . producen fallos en swell

--- src/app/texto/texto.component.ts
+++ src/app/texto/texto.component.ts
@@ -5,6 +5,7 @@ import { isUndefined } from 'util';
 import { RevisionComponent } from '../revision/revision.component';
 import { UserService } from '../user.service';
 
+declare let window: any; // para depuracion sucia en consola
 declare let document: any;
 declare let swell: any;
 
@@ -23,82 +24,100 @@ export class TextoComponent implements OnInit, OnDestroy {
   anotacion: any;
   idComentario: string;
 
-  constructor(private docService: DocService, private service: SwellService, private general: RevisionComponent, private user: UserService) { }
+  constructor(private docService: DocService, private service: SwellService, 
+    private general: RevisionComponent, private user: UserService) { }
 
-  selectionHandler(range, editor, selection){
-    console.log(range);
-    //recoge anotaciones en ese rango si las tiene;
+
+  /**
+   * Crea el nodo donde almacenar comentarios si es necesario.
+   * TODO esta función puede ser estática, moverla a una clase o servicio 
+   * con la lógica de negocio de nuestro objeto
+   * @param object
+   */
+  configuraNodoComentarios(object: any) {
+    if (!object.node('comments')) {
+      object.set('comments', this.service.getSwell().Map.create());
+      console.log('ha creado comments');
+    } else {
+      console.log('Comments ya existia');
+    }
+
+  }
+
+  /**
+   * Crea un nodo dentro de object.node('comments') donde
+   * almacenar el hilo de comentarios con el id correspondiente
+   * @param object
+   */
+  configuraNodoDeComentario(object: any, idComentario: string) {
+
+
+    if (object.node('comments').node(idComentario)) {
+      return;
+    }
+
+    // para nodos de tipo map usar mejor métodos put() y pick()
+    object.node('comments').put(idComentario, this.service.getSwell().Map.create());
+    const comentarioMap = object.node('comments').node(idComentario);
+
+    // para asegurarnoºs de que se crean todas las propiedades correctamente,
+    // crear las propiedades del hilo de comentario en llamadas separadas
+    comentarioMap.put('id', idComentario);
+    comentarioMap.put('posts', this.service.getSwell().List.create());
+
+    console.log('Configurado nodo para comentario ' + idComentario);
+    console.log(comentarioMap.get());
+  }
+
+  selectionHandler(range, editor, selection) {
+
+    console.log('Seleccion capturada en rango ' + range);
+    // recoge anotaciones en ese rango si las tiene;
     const anotComentarios = this.editor.getAnnotations('comment').comment;
-    
-    //si hay anotacion y dentro de la anotacion hay comentarios concretos.
+
+    // si hay anotacion y dentro de la anotacion hay comentarios concretos.
     const tieneComentarios = anotComentarios && Array.isArray(anotComentarios) && anotComentarios.length > 0;
 
-    console.log('Seleccion hecha. Comentarios? ' + tieneComentarios);
+    console.log('Comentarios en la seleccion? ' + tieneComentarios);
     console.log(anotComentarios);
 
-    if(range.start != range.end){
-      if (!tieneComentarios) {
-        console.log("if");
-        // OJO, user siempre mismo clave 'comment'
-        // para convertir a string, sobra con concatenar con '',
-        // aunque he añadido un prefijo 'c' para mejorar comprension
-        this.idComentario = 'c' + (Math.random() * 10000);
-
-        // otro posible id podría ser el timestamp
-        // const idComentario = 'c' + (new Date()).getTime();
-
-        // la anotacion nos devuelve el setAnnotation()
-        this.anotacion = this.editor.setAnnotation('comment', this.idComentario);
-
-        // (Preguntar a pablo porque 2 veces el id)
-        // respuesta: repito el id por si pasas una referencia al objeto poder
-        // saber siempre que comentario es
-
-        // ojo, uso el swell.Map.create() para poder escuchar eventos en posts[]
-        
-        if (!this.object.node("comments")){
-
-          this.object.set('comments', this.service.getSwell().Map.create());
-          console.log("ha creado comments");
-
-
-
-        }else{
-          console.log("Comments ya existia");
-        }
-
-        console.log('creado comments se supone');
-        
-
-        /*this.object.node('comments').set(this.idComentario, this.service.getSwell().Map.create());
-        console.log(this.object.node('coments').node(this.idComentario).value());
-        this.object.node('comments').node(this.idComentario).set('id', this.idComentario);
-        this.object.node('comments').node(this.idComentario).set('posts', this.service.getSwell().List.create());*/
-        this.object.node('comments').set(this.idComentario, this.service.getSwell().Map.create(
-        { 
-          id: this.idComentario,
-          posts: this.service.getSwell().List.create(),
-            
-        }));
-        console.log("Llega aqui se supone que existe comments");
-
-      } else {
-        this.anotacion = anotComentarios[0];
-        this.service.setAnotacion(this.anotacion.value);
-        console.log(this.object.node("comments"));
-        console.log("else" + this.anotacion.value);
-        // muestra a la derecha los comentarios y en caso de poder comentar, el panel para ello.
-        // mostrarComentarios();
-      }
-
-      console.log("llamando a opinar");
-      this.service.setObject(this.object);
-      this.general.opinar();
-      
-    }else{
-       console.log("seleccion de caca");
-       this.general.noOpinar();
+    // selección vacia, no hacer nada
+    if (range.start === range.end) {
+      console.log('seleccion de caca');
+      this.general.noOpinar();
+      return;
     }
+
+
+    if (!tieneComentarios) {
+      // OJO, user siempre mismo clave 'comment'
+      // para convertir a string, sobra con concatenar con '',
+      // aunque he añadido un prefijo 'c' para mejorar comprension
+      this.idComentario = 'c' + parseInt('' + (Math.random() * 10000), 10);
+
+      // otro posible id podría ser el timestamp
+      // const idComentario = 'c' + (new Date()).getTime();
+
+      // la anotacion nos devuelve el setAnnotation()
+      this.anotacion = this.editor.setAnnotation('comment', this.idComentario);
+
+      this.configuraNodoComentarios(this.object);
+
+      this.configuraNodoDeComentario(this.object, this.idComentario);
+
+
+    } else {
+      this.anotacion = anotComentarios[0];
+      // (pablo) estas duplicando la anotacion, es eso lo que quieres hacer aqui ¿?
+      // this.service.setAnotacion(this.anotacion.value);
+      console.log('Ya existe comentario en la seleccion ' + this.anotacion.value);
+    }
+
+    console.log('llamando a opinar');
+    this.service.setObject(this.object);
+    this.general.opinar();
+
+
     // Todo esto va ahora en una funcion a parte, ya que es añadir un comentario
     // this.anotacion.value es donde está el id del comentario
 
@@ -109,6 +128,7 @@ export class TextoComponent implements OnInit, OnDestroy {
   ngOnInit() {
 
     this.object = this.service.getObject();
+    window.object = this.object; // Solo para depurar
     this.service.getSwell().Editor.configure({});
 
     // define() requiere tres parametros
@@ -121,15 +141,15 @@ export class TextoComponent implements OnInit, OnDestroy {
       // despues de crear el editor.
       // para simplificar el codigo fuente, llevar el codigo
       // del handler a un metodo
-      
-      this.editor.setSelectionHandler((range, editor, selection)=>{
-        
-        //this.editor.clearAnnotation('comment');
-        //this.object.delete('comments');
+
+      this.editor.setSelectionHandler((range, editor, selection) => {
+
+        // this.editor.clearAnnotation('comment');
+        // this.object.delete('comments');
         this.selectionHandler(range, editor, selection);
 
       });
-      
+
     }
 
     this.text = this.service.getSwell().Text.create('Texto vacio');
@@ -137,10 +157,9 @@ export class TextoComponent implements OnInit, OnDestroy {
 
     if (!this.object.node('text')) {
       this.object.set('text', this.text);
-      
     }
 
-    console.log(this.object.node("text"));
+    console.log(this.object.node('text'));
     this.text = this.service.getObject().get('text');
     this.editor.set(this.text);
     this.editor.edit(true);
